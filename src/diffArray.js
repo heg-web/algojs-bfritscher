99bdef77d3384870a41d242429c03ede16865213
Boris Fritscher <boris@fritscher.ch>
2022-06-29 11:50:47 +0200

    diffarray

--- src/diffArray.js
+++ src/diffArray.js
@@ -7,10 +7,27 @@ In other words, return the symmetric difference of the two arrays.
 Note:
 You can return the array with its elements in any order.
 
-*/
-function diffArray(arr1, arr2) {
 
-  return;
+
+*/
+function diffAB(a, b) {
+  const result = [];
+  for (let i = 0; i < a.length; i++) {
+    const element = a[i];
+    if (!b.includes(element)) {
+      result.push(element);
+    }
+  }
+  return result;
+}
+
+
+function diffArray(arr1, arr2) {
+  // loop arr1 and find elements not in arr2
+  const ab = diffAB(arr1, arr2);
+  // loop arr2 and find elements not in arr1
+  const ba = diffAB(arr2, arr1);
+  return ab.concat(ba);
 }
 
 exports.diffArray = diffArray;
